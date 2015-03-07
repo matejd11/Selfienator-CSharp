ae813c19dbd89d99a5b101e7a500d21ae1067283
MatejD <mdujava@gmail.com>
2015-03-07 23:26:17 +0100

    case if there are not any avaible port

--- Arduino.Selfienator/Arduino.Selfienator/ViewModels/LoadWindowViewModel.cs
+++ Arduino.Selfienator/Arduino.Selfienator/ViewModels/LoadWindowViewModel.cs
@@ -14,19 +14,35 @@ namespace Arduino.Selfienator.ViewModels
     {
         private string[] _listOfPorts;
         private string _selectedPort;
+        private bool _isEditAllowed;
 
         public LoadWindowViewModel()
         {
             listOfPorts = SerialPort.GetPortNames();
         }
 
-        public ICommand connectComm { get { return new ActionCommand(connect); } }
+        public ICommand connectComm { get { return new ActionCommand(connect, canConnect); } }
+
+        public ICommand refreshComm { get { return new ActionCommand(refresh); } }
+
+        private void refresh(object obj)
+        {
+            listOfPorts = SerialPort.GetPortNames();
+        }
+
+        private bool canConnect(object obj)
+        {
+            if (!String.IsNullOrWhiteSpace((string)obj) && (string)obj != "nenájdené")
+            {
+                return true;
+            }
+            return false;
+        }
 
         private void connect(object obj)
         {
             string portName = (string)obj;
 
-            MessageBox.Show(portName);
 
         }
 
@@ -36,6 +52,20 @@ namespace Arduino.Selfienator.ViewModels
             set
             {
                 _listOfPorts = value;
+                if (value.Length == 0)
+                {
+                    _listOfPorts = new string[] {"nenájdené"};
+                }
+                if (_listOfPorts.Length == 1)
+                {
+                    selectedPort = _listOfPorts[0];
+                    isEditAllowed = false;
+                }
+                else
+                {
+
+                    isEditAllowed = true;
+                }
                 NotifyPropertyChanged();
             }
         }
@@ -46,6 +76,15 @@ namespace Arduino.Selfienator.ViewModels
             set
             {
                 _selectedPort = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public bool isEditAllowed
+        {
+            get { return _isEditAllowed; }
+            set
+            {
+                _isEditAllowed = value;
                 NotifyPropertyChanged();
             }
         }
