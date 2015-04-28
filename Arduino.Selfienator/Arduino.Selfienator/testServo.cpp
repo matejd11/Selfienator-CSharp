0742ec27d4d91b5e83d65e8b7d822ce1145e5bf7
MatejD <mdujava@gmail.com>
2015-04-28 19:22:03 +0200

    testServo

---
+++ Arduino.Selfienator/Arduino.Selfienator/testServo.cpp
@@ -0,0 +1,26 @@
+﻿#include <Servo.h> 
+
+Servo myservo;  // create servo object to control a servo 
+
+int myservoPin = 4;
+
+void setup()
+{
+	Serial.begin(9600);
+	myservo.attach(myservoPin);
+}
+
+
+void loop()
+{
+	for (int i = 0; i < 180; i++)
+	{
+		myservo.write(i);
+		sleep(25);
+	}
+	for (int i = 180; i > 0; i--)
+	{
+		myservo.write(i);
+		sleep(25);
+	}
+}
